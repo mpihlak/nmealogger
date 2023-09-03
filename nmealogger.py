18258b5aaff297ca211972cbb55e8f0d2fdae92c
Martin Pihlak <martin.pihlak@starship.co>
2023-09-03 20:37:52 +0300

    Python 3.5 compatibility
    Turns out that Raspberry stretch has Python 3.5 which does not
    have f-strings yet.

--- nmealogger.py
+++ nmealogger.py
@@ -30,8 +30,8 @@ def valid_nmea_checksum(sentence):
 def read_and_log(s):
     while True:
         file_ts = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-        filename = os.path.join(OUTPUT_DIRECTORY, f"nmea-{file_ts}.log")
-        logging.info(f"Writing to {filename}")
+        filename = os.path.join(OUTPUT_DIRECTORY, "nmea-{file_ts}.log".format(file_ts=file_ts))
+        logging.info("Writing to " + filename)
 
         ok_sentences = 0
         nok_sentences = 0
@@ -48,31 +48,32 @@ def read_and_log(s):
                 
                 strdata = data.decode('ascii', errors='ignore')
                 if not strdata.endswith('\n'):
-                    logging.warning(f"Data does not end in newline: {strdata}")
+                    logging.warning("Data does not end in newline: " + strdata)
                 else:
                     strdata = strdata.rstrip()
                 sentences = strdata.split('\n')
 
                 ts = datetime.datetime.now(datetime.timezone.utc).isoformat()
                 for sentence in sentences:
+                    sentence = sentence.strip("\r")
                     if valid_nmea_checksum(sentence):
-                        f.write(f"{ts}\t{sentence}\n".encode('ascii'))
+                        f.write((ts + "\t" + sentence + "\n").encode('ascii'))
                         f.flush()
                         ok_sentences += 1
                     else:
-                        logging.warning(f"Skipping sentence with invalid checksum: {sentence}")
+                        logging.warning("Skipping sentence with invalid checksum: " + sentence)
                         nok_sentences += 1
 
                 now = time.time()
 
                 if now - last_reported_time > REPORTING_INTERVAL_SECONDS:
-                    logging.info(f"{ok_sentences} NMEA sentences logged, {nok_sentences} skipped.")
+                    logging.info("{ok_sentences} NMEA sentences logged, {nok_sentences} skipped.".format(ok_sentences=ok_sentences, nok_sentences=nok_sentences))
                     last_reported_time = now
                     ok_sentences = 0
                     nok_sentences = 0
 
                 if now - last_rotation_time > ROTATION_INTERVAL_SECONDS:
-                    logging.info(f"Rotation interval of {ROTATION_INTERVAL_SECONDS} seconds elapsed.")
+                    logging.info("File rotation interval elapsed.")
                     break
 
 if __name__ == "__main__":
@@ -82,22 +83,22 @@ if __name__ == "__main__":
         datefmt='%Y-%m-%dT%H:%M:%SZ'
     )
 
-    logging.info(f"Starting NMEA logger")
+    logging.info("Starting NMEA logger")
 
     if not os.path.exists(OUTPUT_DIRECTORY):
-        logging.info(f"Output directory [{OUTPUT_DIRECTORY}] does not exist, creating.")
+        logging.info("Output directory does not exist, creating " + OUTPUT_DIRECTORY)
         os.mkdir(OUTPUT_DIRECTORY)
 
     while True:
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                logging.info(f"Connecting to {HOST}:{PORT}")
+                logging.info("Connecting to {HOST}:{PORT}".format(HOST=HOST,PORT=PORT))
                 s.connect((HOST, PORT))
                 read_and_log(s)
         except ConnectionRefusedError:
-            logging.error(f"Connection refused")
+            logging.error("Connection refused")
         except Exception as e:
-            logging.exception(f"Exception occurred: {e}")
+            logging.exception("Exception occurred: " + str(e))
         finally:
-            logging.info(f"Retrying after {RETRY_INTERVAL_SECONDS} second(s).")
+            logging.info("Retrying ...")
             time.sleep(RETRY_INTERVAL_SECONDS)
