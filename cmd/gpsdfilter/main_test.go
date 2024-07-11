5b6649e52c02c4087cb3006f67ae1579fe275a78
Martin Pihlak <martin.pihlak@starship.co>
2024-07-11 10:59:07 +0300

    Add a GPSD T122 compat daemon
    Some USB GPSs output NMEA sentences that are incompatible with what the
    TackTick T122 expects. Add a filtering daemon to rewrite these
    sentences.

---
+++ cmd/gpsdfilter/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestRewritePosition(t *testing.T) {
+	sentence := "$GPGLL,5928.09769,N,02449.29616,E,180019.00,A,A*6E"
+	expected := "$GPGLL,5928.098,N,02449.296,E,180019,A"
+	result := rewritePosition(sentence)
+	if result != expected {
+		t.Fatalf("Expected [%s], got [%s]", expected, result)
+	}
+}
+
+func TestRewriteNavigationInfo(t *testing.T) {
+	sentence := "$GPRMC,180021.00,A,5928.09760,N,02449.29615,E,0.031,,090724,,,A*7C"
+	expected := "$GPRMC,180021,A,5928.098,N,02449.296,E,00.0,,090724,00,E"
+	result := rewriteNavigationInfo(sentence)
+	if result != expected {
+		t.Fatalf("Expected [%s], got [%s]", expected, result)
+	}
+}
